c8f3c255dc46716d2543559f79c3d1a085b3f2b8
Richard Barrett <rich@parcelmonkey.com>
2018-07-09 14:31:13 +0100

    bugfix

--- src/Track.php
+++ src/Track.php
@@ -8,6 +8,9 @@ class Track extends ApiResource {
   
   static function get($barcode, $postcode) {
     
+    $barcode = urlencode($barcode);
+    $postcode = urlencode($postcode);
+    
     $r = self::init();
     $r->method = 'GET';
     $r->uri.="/$barcode/$postcode";
