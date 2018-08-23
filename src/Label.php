b4b6f8689c4a87a05d21500007671b3c51ba259d
Richard Barrett <rich@parcelmonkey.com>
2018-08-23 15:34:11 +0100

    support label versions

--- src/Label.php
+++ src/Label.php
@@ -6,12 +6,12 @@ class Label extends ApiResource {
   
   var $uri = '/Label';
   
-  static function get($id, $format='base64') {
+  static function get($id, $format='base64', $version='1') {
     
     $r = self::init();
     $r->method = 'GET';
     if($id) $r->uri.="/$id";
-    if($format) $r->uri.="/$format";
+    if($format) $r->uri.="/$format/$version";
     return $r->sendRequest();
     
   }
